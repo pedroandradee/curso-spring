ded01e6a93544c906aa3eef2f533ea1292680fe4
pedroandradee <pedro.h8237@gmail.com>
2022-09-23 10:17:04 -0300

    Product creation and association many to many

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.Category;
+import com.example.demo.domain.Product;
 import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.repositories.ProductRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -25,7 +29,19 @@ public class DemoApplication implements CommandLineRunner {
 		Category c1 = new Category(null, "Computing");
 		Category c2 = new Category(null, "Office");
 		
+		Product p1 = new Product(null, "Computer", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		c1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(c1));
+		p2.getCategories().addAll(Arrays.asList(c1, c2));
+		p3.getCategories().addAll(Arrays.asList(c1));
+		
 		categoryRepository.saveAll(Arrays.asList(c1, c2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 	
