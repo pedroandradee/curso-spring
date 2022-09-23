dbab8b105ca1c9ef6b61a258d4e499e19da0e24c
pedroandradee <pedro.h8237@gmail.com>
2022-09-23 08:49:50 -0300

    first domain class testing

--- src/main/java/com/example/demo/resourses/CategoriesResource.java
+++ src/main/java/com/example/demo/resourses/CategoriesResource.java
@@ -1,16 +1,28 @@
 package com.example.demo.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoriesResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Categories";
+	public List<Category> list() {
+		Category c1 = new Category(1, "Computing");
+		Category c2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(c1);
+		list.add(c2);
+		
+		return list;
 	}
 	
 }
