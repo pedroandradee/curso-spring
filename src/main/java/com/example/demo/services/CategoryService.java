f7ba81956dc9abef84b96aa384a1fb100d1276fc
pedroandradee <pedro.h8237@gmail.com>
2022-09-23 10:43:06 -0300

    enpoint /category/{id} adjustments

--- src/main/java/com/example/demo/services/CategoryService.java
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.example.demo.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Category;
 import com.example.demo.repositories.CategoryRepository;
-import java.util.Optional;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -15,7 +17,8 @@ public class CategoryService {
 	
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+			"Object cannot be find! Id: " + id + ", Type: " + Category.class.getName()));
 	}
 	
 }
