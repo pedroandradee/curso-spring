d8f44f736dc58f3e57b5e2e79abc1ff2652f816d
pedroandradee <pedro.h8237@gmail.com>
2022-09-23 09:50:47 -0300

    instancing operation creating

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.domain.Category;
+import com.example.demo.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category c1 = new Category(null, "Computing");
+		Category c2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(c1, c2));
+	}
+	
+	
 
 }
