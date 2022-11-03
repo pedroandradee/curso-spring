f77a08660d00e1f89f8799ef3704107b2281f706
pedroandradee <pedro.h8237@gmail.com>
2022-11-03 12:30:04 -0300

    Swagger documentation - part 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class DemoApplication implements CommandLineRunner {
 	
 	
